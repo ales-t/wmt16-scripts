b40dd560b88385eae74402cb04253ebcdfe3f643
Ales Tamchyna <tamchyna@ufal.mff.cuni.cz>
2017-02-07 15:09:21 +0100

    export

--- train.sh
+++ train.sh
@@ -3,4 +3,9 @@
 mydir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $mydir/settings.sh
 
+export SRC
+export TGT
+export DEV
+export TRAIN
+
 THEANO_FLAGS=mode=FAST_RUN,floatX=float32,device=$DEVICE,on_unused_input=warn python config.py
